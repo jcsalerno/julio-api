21eb7064ae0db6b2c58af4d8494dcd3c9b2990c3
Julio Cesar <contato.jcsalerno@gmail.com>
2024-06-28 19:33:39 -0300

    feat: creation package domain and User class

---
+++ src/main/java/br/com/julio/api/domain/User.java
@@ -0,0 +1,19 @@
+package br.com.julio.api.domain;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
