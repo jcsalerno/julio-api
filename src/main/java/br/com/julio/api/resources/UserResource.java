0bffce1d288bf7d330644c418b6f677e8e4cd438
Julio Cesar <contato.jcsalerno@gmail.com>
2024-06-28 21:38:55 -0300

    feat: User Resource class

--- src/main/java/br/com/julio/api/resources/UserResource.java
+++ src/main/java/br/com/julio/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.julio.api.resources;
 import br.com.julio.api.domain.User;
+import br.com.julio.api.domain.dto.UserDTO;
 import br.com.julio.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
