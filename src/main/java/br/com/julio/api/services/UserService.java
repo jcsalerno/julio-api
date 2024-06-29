dd4c53c9be535cec15aa0e08660ccc908814d659
Julio Cesar <contato.jcsalerno@gmail.com>
2024-06-29 18:43:45 -0300

    feat: endpoint for findAll User

--- src/main/java/br/com/julio/api/services/UserService.java
+++ src/main/java/br/com/julio/api/services/UserService.java
@@ -1,7 +1,10 @@
 package br.com.julio.api.services;
 import br.com.julio.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
--- src/main/java/br/com/julio/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/julio/api/services/impl/UserServiceImpl.java
@@ -5,7 +5,7 @@ import br.com.julio.api.services.UserService;
 import br.com.julio.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,11 +14,13 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
-
-
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<User> findAll(){
+        return repository.findAll();
     }
 }
