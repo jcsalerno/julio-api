bb5a3c7d8452d8ff06333669f1ddb572f6682a61
Julio Cesar <contato.jcsalerno@gmail.com>
2024-07-01 20:36:25 -0300

    feat: test for class user service method delete with success

--- src/test/java/br/com/julio/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/julio/api/services/impl/UserServiceImplTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -142,7 +142,12 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
+
     }
 
     private void startUser() {
