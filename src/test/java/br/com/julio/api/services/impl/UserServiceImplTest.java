9b4092e0819fd04503166f5e308b4557c77c450e
Julio Cesar <contato.jcsalerno@gmail.com>
2024-07-01 20:12:19 -0300

    feat: test for class user service method create return success

--- src/test/java/br/com/julio/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/julio/api/services/impl/UserServiceImplTest.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -90,7 +91,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(MAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
