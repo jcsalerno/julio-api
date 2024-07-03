eb0ab9dec23bcfe3a16c3a5f0d13371068e11144
Julio Cesar <contato.jcsalerno@gmail.com>
2024-07-02 21:53:55 -0300

    fix: code correction and optimization

--- src/main/java/br/com/julio/api/resources/exceptions/StandardError.java
+++ src/main/java/br/com/julio/api/resources/exceptions/StandardError.java
@@ -1,14 +1,12 @@
 package br.com.julio.api.resources.exceptions;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+
 
 import java.time.LocalDateTime;
 
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class StandardError {
 
