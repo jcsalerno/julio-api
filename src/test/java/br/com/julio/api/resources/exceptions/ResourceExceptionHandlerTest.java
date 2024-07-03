ff3af693a8f9e0c2805f44960088716fa8a2cbee
Julio Cesar <contato.jcsalerno@gmail.com>
2024-07-02 21:55:18 -0300

    feat: teste for execption handler

--- src/test/java/br/com/julio/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/julio/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,20 +1,64 @@
 package br.com.julio.api.resources.exceptions;
 
+import br.com.julio.api.services.exceptions.DataIntegratyViolationException;
+import br.com.julio.api.services.exceptions.ObjectNotFoundException;
+import org.apache.coyote.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 class ResourceExceptionHandlerTest {
+
+    @InjectMocks
+    private ResourceExceptionHandler exceptionHandler;
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void objectNotFound() {
+    void whenObjectNotFoundExceptionThenReturnAResponseEntity() {
+
+        ResponseEntity<StandardError> response = exceptionHandler
+                .objectNotFound(
+                        new ObjectNotFoundException("ID not found"),
+                        new MockHttpServletRequest()
+                );
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, Objects.requireNonNull(response.getBody()).getClass());
+        assertEquals("ID not found", response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegrityViolationException(
+                        new DataIntegratyViolationException("Email already registered in the system"),
+                        new MockHttpServletRequest()
+                );
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, Objects.requireNonNull(response.getBody()).getClass());
+        assertEquals("Email already registered in the system", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
