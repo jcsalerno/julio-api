d087e41a90f1ccf65fb6061ddb2b8eb295f68879
Julio Cesar <contato.jcsalerno@gmail.com>
2024-07-02 21:40:43 -0300

    feat: exception handling test method id not found

---
+++ src/test/java/br/com/julio/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -0,0 +1,20 @@
+package br.com.julio.api.resources.exceptions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ResourceExceptionHandlerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void objectNotFound() {
+    }
+
+    @Test
+    void dataIntegratyViolationException() {
+    }
+}
